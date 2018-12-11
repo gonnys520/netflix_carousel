736b24abe499adff091199041b7252b5ef02db00
gonnys <gonnys520@gmail.com>
2018-12-11 18:59:04 +0900

    sample demo

---
+++ src/main/java/org/gorany/CarouselSlidersApplication.java
@@ -0,0 +1,12 @@
+package org.gorany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarouselSlidersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarouselSlidersApplication.class, args);
+	}
+}
